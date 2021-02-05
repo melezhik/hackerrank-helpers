fd41b603f17860704b3831a3b2d1db41b3e8ab41
Alexey Melezhik <melezhik@gmail.com>
2021-02-05 14:43:36 -0600

    Create code.pl

--- fraudulent-activity-notifications/code.pl
+++ fraudulent-activity-notifications/code.pl
@@ -8,6 +8,9 @@ use Data::Dumper;
 our $LOGME = 1;
 our @heap;
 our $heapSize;
+our @dd_sorted;
+our $fd_index = 0;
+our $fd;
 
 sub dumparray {
     return unless $LOGME;    
@@ -30,11 +33,37 @@ sub logme {
 
 # Complete the activityNotifications function below.
 
+sub add {
+    my $value = shift;
+    
+    push @heap, $value;
+    my $hs = scalar @heap;
+    my $i = $hs - 1;
+    
+    my $parent = int(($i - 1) / 2);
+
+    while ($i > 0 && $heap[$parent] < $heap[$i])
+    {
+        my $temp = $heap[$i];
+
+        $heap[$i] = $heap[$parent];
+        
+        $heap[$parent] = $temp;
+
+        $i = $parent;
+        
+        $parent = int(($i - 1) / 2);
+        
+    }
+}
+
 sub heapify {
     
     my $i = shift;
+    my $track_fd =  shift || 0;
     
-    logme("run heapify for i",$i);
+    #logme("run heapify for i",$i);
+
     my $hs = scalar @heap;
     
     my $leftChild;
@@ -46,15 +75,8 @@ sub heapify {
         $leftChild = 2 * $i + 1;
         $rightChild = 2 * $i + 2;
         $largestChild = $i;
-      
-        
+              
         if ($leftChild < $hs) {
-            #logme('$leftChild < $heapSize',"$leftChild|$heapSize");
-            #logme('$leftChild',$leftChild);
-            #logme('$heap[$leftChild]',$heap[$leftChild]);
-            #logme('$leftChild',$leftChild);
-            #logme('$heap[$leftChild]',$heap[$leftChild]);
-            #logme('heapSize',$heapSize); 
             if ($heap[$leftChild] > $heap[$largestChild]) 
             {
                 $largestChild = $leftChild;
@@ -72,12 +94,18 @@ sub heapify {
         }
 
         my $temp = $heap[$i];
+        $fd_index = $i if $track_fd && $fd == $temp; # track first day index in heap
         $heap[$i] = $heap[$largestChild];
+        $fd_index = $largestChild if $track_fd && $fd == $heap[$largestChild]; # track first day index in heap
         $heap[$largestChild] = $temp;
         $i = $largestChild;
             
     }
-    
+
+    if ($track_fd){
+        $fd_index = $i if $fd == $heap[$largestChild]; # track first day index in heap
+    }
+
 }
 
 sub getMax {
@@ -90,15 +118,49 @@ sub heapSort {
 
     my @r;
 
+    my @tmp =  @heap;
+
     for (my $i = (scalar @heap) - 1; $i >= 0; $i--)
     {
         $r[$i] = getMax();
         heapify(0);
     }
-        
+    
+    @heap = @tmp;
+
     return @r;
+} 
+
+sub build_heap {
+
+    my $hs = scalar @heap;
+
+    for (my $i = int($hs / 2); $i >= 0; $i--)
+    {
+        heapify($i,1);
+    }
+
 }
 
+sub mediana {
+
+    my $m;
+    my $i = int($heapSize/2);
+    my $j =  $i - 1;
+    
+    if ($heapSize % 2 ==  1){
+        $m = $dd_sorted[$i];
+        logme("heapSize is not even, middle index", $i);
+        logme("heapSize is not even, m", $m);
+    } else {
+        $m = ($dd_sorted[$i] + $dd_sorted[$j]) / 2;
+        logme("heapSize is even, middle indexes", "$j:$i ($dd_sorted[$i]:$dd_sorted[$j])");
+        logme("heapSize is even, m", $m);
+    }
+
+    return $m;
+
+}
 sub activityNotifications {
 
    my $ntf = 0;
@@ -118,26 +180,85 @@ sub activityNotifications {
        
    }
    
-   our $heapSize = $d + 0;
+    our $heapSize = $d + 0;
 
-   our @heap = @$days[0..$heapSize-1];
+    our @heap = @$days[0..$heapSize-1];
  
-    dumparray("unsorted heap",@heap);
-    
-    
+    dumparray("initial heap",@heap);
+
+    $fd = $heap[0]; # remember first day
+
     # create binary heap from first $d elements of @$days
-    for (my $i = int($heapSize / 2); $i >= 0; $i--)
-    {
-        heapify($i);
-    }
- 
-   dumparray("semi sorted heap",@heap);
-   
-   my @dd_sorted = heapSort();
+    build_heap();
 
-   dumparray("last $d sorted days",@dd_sorted);
+    dumparray("heap",@heap);
+
+    logme("initial state", "(fd=$fd, index_in_heap:$fd_index)");
+
+    @dd_sorted = heapSort();
+
+    dumparray("last $d sorted days",@dd_sorted);
+
+    my $m = mediana();
+
+    my $k = 0;
+   
+    for my $i (@$days){
+
+       $k++;
+
+       next unless $k > $d; # start counting frauds after $d days
+
+       #print "process $k ...\n";
+
+       logme(">>>","=================");
+
+       logme("[$k] process day:", "$i (fd=$fd, index_in_heap:$fd_index) (m=$m)"); 
+
+       if ($i >= 2*$m){
+            $ntf++;
+            logme("bump notification counter, suspisiously high expenses for this day",$ntf);
+        }
+
+       if ($i == $fd) {
+
+           logme("current day: $d equal the first day: $fd", "don't recalculate heap, sorted days and mediana");
+
+           # do noting if the current day expense as the first day expense
+           # because this does not change mediana
+
+            #dumparray("[k] heap",@heap);
+            #dumparray("[k] last $d sorted days",@dd_sorted);
+
+       } else {
+
+            # subsitute the first day in the heap by the current day
+            # then restore heaps properties
+            # and recalculate mediana
+            
+            logme("rebuild heap", "replace [$fd] by [$i]");
+
+            #for my $j (0 .. (scalar @heap)-1){
+            #    if ($heap[$j] == $fd){
+                    $heap[$fd_index] = $i;
+                    heapify($fd_index,1);
+                    @dd_sorted = heapSort(); 
+                    #dumparray("[u] heap",@heap);
+                    #dumparray("[u] last $d sorted days",@dd_sorted);
+                    $m = mediana();
+                    $fd = $i;
+                    #last;
+            #    }
+            #}
+
+       } 
+ 
+ 
+    }
     
-   return $ntf;
+    logme("total notifications to be send:", $ntf);
+
+    return $ntf;
 }
 
 open(my $fptr, '>', $ENV{'OUTPUT_PATH'});
